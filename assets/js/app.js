987f7e9639959a99038207136bd4bfc73b0076eb
azerahmedov <ehmedovh111@gmail.com>
2021-11-14 17:27:20 +0400

    xetalar duzeldildi

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,7 @@ let mobil_list = document.querySelector('.listMobil');
 open_bar.addEventListener('click', () => {
   open_bar.classList.toggle('bx-x')
   mobil_list.classList.toggle('active')
+  Body.classList.toggle('active')
 })
+
+let Body = document.querySelector('#body');
